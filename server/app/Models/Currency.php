97c2882526c5c76ea2b2917644cde1c0dc4d2c9b
Mahmed Esam <mahmedesam26@gmail.com>
2026-09-02 17:58:06 +0300

    update currency

--- server/app/Models/Currency.php
+++ server/app/Models/Currency.php
@@ -4,13 +4,13 @@ namespace App\Models;
 class Currency extends BaseModel{
     protected string $table = 'currency';
 
-    protected string $primaryKey = 'currency_type_id ';
+    protected string $primaryKey = 'currency_type_id';
 
     protected array $fillable = [
         'currency_type_name',
         'currency_abbre',
         'Name_EN',
-        'country_abbrev ',
+        'country_abbrev',
         'currency_abbrev',
     ];
 }
